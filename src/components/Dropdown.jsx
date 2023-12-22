7bf74bee2316fd6853b21eea37d9b344c1b9aafb
kitsuiwebster <raphmartin1999@gmail.com>
2023-12-22 18:50:45 +0100

    feat: dropdown open toggle

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,7 +1,21 @@
-function Dropdown() {
+import '../assets/scss/components/Dropdown.scss';
+import {useState} from "react";
+
+function Dropdown({children, title}) {
+
+    const [open, setOpen] = useState(false);
+    const toggle = () => {
+        setOpen(!open);
+    }
+
     return(
-        <div>
-            <h2>Équipements</h2>
+        <div className={`dropdown ${open?"":"dropdown--reduced"}`} onClick={toggle}>
+            <div className={'dropdown--title'}>
+                {title}
+            </div>
+            <div className={'dropdown--content'}>
+                {children}
+            </div>
         </div>
     )
 }
