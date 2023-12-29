112df95d19b4ae2be3578b450a58ed2979d90be3
kitsuiwebster <raphmartin1999@gmail.com>
2023-12-29 16:31:15 +0100

    feat: 404 page structure

---
+++ src/pages/not-found/index.jsx
@@ -0,0 +1,11 @@
+function NotFound() {
+    return(
+        <div>
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <a href="/">Retournez sur la page d'accueil</a>
+    </div>
+    )
+}
+
+export default NotFound
