177bca44be1568fdaa9d17999631794bac196a61
kitsuiwebster <raphmartin1999@gmail.com>
2024-01-18 23:54:36 +0100

    feat: 404 page scss

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -1,10 +1,12 @@
+import './index.scss';
+
 function NotFound() {
     return(
-        <div>
-        <h1>404</h1>
-        <p>Oups! La page que vous demandez n'existe pas.</p>
-        <a href="/">Retournez sur la page d'accueil</a>
-    </div>
+        <div className="not-found-container">
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <a href="/">Retournez sur la page d'accueil</a>
+        </div>
     )
 }
 
