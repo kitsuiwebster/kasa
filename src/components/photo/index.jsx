59d8cded3ff2bc3bcb25db19772008013bce3d19
kitsuiwebster <raphmartin1999@gmail.com>
2024-01-20 11:58:03 +0100

    feat: housing photos galery

--- src/components/photo/index.jsx
+++ src/components/photo/index.jsx
@@ -1,11 +1,34 @@
+import React, { useState } from 'react';
 import './index.scss';
 
-function Photo({ imageSrc, title }) {
-    return(
+function Photo({ imageSrcs, title }) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const hasImages = imageSrcs && imageSrcs.length > 0;
+
+    const handleNext = () => {
+        if (hasImages) {
+            setCurrentIndex((currentIndex + 1) % imageSrcs.length);
+        }
+    };
+
+    const handlePrev = () => {
+        if (hasImages) {
+            setCurrentIndex((currentIndex - 1 + imageSrcs.length) % imageSrcs.length);
+        }
+    };
+
+    if (!hasImages) {
+        return <div>Aucune photo disponible.</div>;
+    }
+
+    return (
         <div className="photo">
-            <img className="photo-file" src={imageSrc} alt={title}></img>
+            <button className="photo-button photo-button-prev" onClick={handlePrev}>&lt;</button>
+            <img className="photo-file" src={imageSrcs[currentIndex]} alt={title} />
+            <button className="photo-button photo-button-next" onClick={handleNext}>&gt;</button>
         </div>
-    )
+    );
 }
 
-export default Photo
+export default Photo;
