15b6424387d0d4d320fa258fcdc195a794a912e1
kitsuiwebster <raphmartin1999@gmail.com>
2024-01-20 11:55:27 +0100

    feat: dropdown animations

--- src/components/dropdown/index.jsx
+++ src/components/dropdown/index.jsx
@@ -1,26 +1,25 @@
 import './index.scss';
-import {useState} from "react";
+import { useState } from "react";
 
-function Dropdown({children, title}) {
-
+function Dropdown({ children, title }) {
     const [open, setOpen] = useState(false);
     const toggle = () => {
         setOpen(!open);
-    }
+    };
 
-    return(
-        <div className={`dropdown`}>
-            <div className={'dropdown--container'}>
-                <div className='dropdown--title'>{title}</div>
-                <div className='dropdown--arrow' onClick={toggle}>
+    return (
+        <div className="dropdown">
+            <div className="dropdown--container" onClick={toggle}>
+                <div className="dropdown--title">{title}</div>
+                <div className={`dropdown--arrow ${open ? "dropdown--arrow-rotated" : ""}`}>
 
                 </div>
             </div>
-            <div className={`dropdown--content ${open ? "" : "dropdown--reduced"}`}>
+            <div className={`dropdown--content ${open ? "dropdown--open" : ""}`}>
                 {children}
             </div>
         </div>
     )
 }
 
-export default Dropdown
+export default Dropdown;
