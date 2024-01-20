d2490c18b7e611e2bd68432bd4aebb8047f612c1
kitsuiwebster <raphmartin1999@gmail.com>
2024-01-20 11:54:56 +0100

    feat: header logo link

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,15 +1,18 @@
 import './index.scss';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return(
         <header className="header">
-            <img src={require("../../assets/images/logo-home.svg").default} alt="Kasa"></img>
+            <Link to="/"> 
+                <img src={require("../../assets/images/logo-home.svg").default} alt="Kasa" />
+            </Link>
             <nav className="header--nav">
                 <a href="/">Accueil</a>
                 <a href="/about">À Propos</a>
             </nav>
         </header>
-    )
+    );
 }
 
 export default Header
